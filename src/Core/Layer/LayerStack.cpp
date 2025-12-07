2a8ece17ac55a8f408d209a6b208eea5293710ad
Terry Wright-ZED <callmeterryv2@gmail.com>
2025-12-07 01:54:43 -0500

    Fixed OnDetach to be called before removing from the stack

--- src/Core/Layer/LayerStack.cpp
+++ src/Core/Layer/LayerStack.cpp
@@ -39,9 +39,9 @@ namespace FPS
         auto it = std::find(m_Layers.begin(), m_Layers.end(), layer);
         if (it != m_Layers.end())
         {
+            layer->OnDetach();
 			m_Layers.erase(it);
 			m_LayerInsertIndex--;
-            layer->OnDetach();
         }
     }
 
@@ -50,8 +50,8 @@ namespace FPS
         auto it = std::find(m_Layers.begin(), m_Layers.end(), overlay);
         if (it != m_Layers.end())
         {
-            m_Layers.erase(it);
             overlay->OnDetach();
+            m_Layers.erase(it);
         }
     }
 }
